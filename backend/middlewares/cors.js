a4a562aa6663e2ad6bd615d5d5acf77b6b89896e
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-08-21 17:49:38 -0500

    feat: fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,7 +1,7 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 const allowedCors = [
-  'https://naumch1k.students.nomoredomains.rocks',
-  'https://api.naumch1k.students.nomoredomains.rocks',
+  'http://naumch1k.students.nomoredomains.rocks',
+  'http://api.naumch1k.students.nomoredomains.rocks',
   'http://localhost:3000',
 ];
 
